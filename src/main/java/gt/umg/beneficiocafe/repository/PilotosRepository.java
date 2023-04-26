88932a25921d09db41fe8d0af8a7a1cada695838
Raymundo Diaz <eiraymun@sat.gob.gt>
2023-04-26 15:59:31 -0600

    se versionan los modelos al ws

---
+++ src/main/java/gt/umg/beneficiocafe/repository/PilotosRepository.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gt.umg.beneficiocafe.repository;
+
+import gt.umg.beneficiocafe.models.BCPilotos;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author Elio Raymundo
+ */
+public interface PilotosRepository extends JpaRepository<BCPilotos, String> {
+    
+}
---
+++ src/main/java/gt/umg/beneficiocafe/services/PilotosService.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gt.umg.beneficiocafe.services;
+
+import gt.umg.beneficiocafe.exceptions.BadRequestException;
+import gt.umg.beneficiocafe.models.BCPilotos;
+import gt.umg.beneficiocafe.payload.request.CrearSolicitudRequest;
+import gt.umg.beneficiocafe.payload.response.SuccessResponse;
+import gt.umg.beneficiocafe.repository.PilotosRepository;
+import gt.umg.beneficiocafe.security.jwt.JwtUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Elio Raymundo
+ */
+
+@Service
+public class PilotosService {
+    private final PilotosRepository pilotosRepository;
+    private JwtUtils jwtUtils;
+    private static final Logger logger = LoggerFactory.getLogger(PilotosService.class);
+
+    public PilotosService(PilotosRepository pilotosRepository, JwtUtils jwtUtils) {
+        this.pilotosRepository = pilotosRepository;
+        this.jwtUtils = jwtUtils;
+    }
+    
+    /*
+        Metodo para crear un piloto
+    */
+    public ResponseEntity<?> registrarPiloto(CrearSolicitudRequest piloto) throws BadRequestException{
+        String respuesta;
+        logger.info("El piloto a crear es " + piloto);
+        try{
+            BCPilotos nuevoPiloto = new BCPilotos();
+            pilotosRepository.save(nuevoPiloto);
+            return ResponseEntity.ok(new SuccessResponse(HttpStatus.OK, "El piloto se creo exitosamente", nuevoPiloto));
+        } catch(BadRequestException e) {
+            throw new BadRequestException(e.getMessage());
+        }
+    }
+    
+    
+}
